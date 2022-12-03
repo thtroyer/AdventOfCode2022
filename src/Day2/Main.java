ed9b5a05584692fce072305df680d2963d7d3d06
Tom Troyer <tom.troyer@gmail.com>
2022-12-03 00:30:28 -0500

    Day 2 second solution

--- src/Day2/Main.java
+++ src/Day2/Main.java
@@ -12,13 +12,15 @@ import java.util.function.Function;
  * A / X - Rock
  * B / Y - Paper
  * C / Z - Scissors
+ * (Not the cleanest code, but I'm trying out some new Java syntax and pushing my knowledge of streams a bit)
  */
 public class Main {
     public static void main(String[] args) {
-        new Main().run();
+        new Main().solution1();
+        new Main().solution2();
     }
 
-    public void run() {
+    public void solution1() {
         var input = readInput("src/Day2/input.txt");
         Function<String, Integer> wonScore = in -> {
             char o = in.charAt(0);
@@ -42,7 +44,7 @@ public class Main {
         var score = Arrays.stream(input.split("\n")).map(
                 l -> {
                     char player = l.charAt(2);
-                    int scoreFromPlay = switch(player) {
+                    int scoreFromPlay = switch (player) {
                         case 'X' -> 1;
                         case 'Y' -> 2;
                         case 'Z' -> 3;
@@ -52,6 +54,77 @@ public class Main {
                     return scoreFromPlay + wonScore.apply(l);
                 }
         ).mapToInt(Integer::intValue).sum();
+
+        System.out.println(score);
+    }
+
+    public void solution2() {
+        var input = readInput("src/Day2/input.txt");
+
+        // X = lose, Y = tie, Z = win
+
+        Function<String, Integer> wonScore = in -> {
+            char o = in.charAt(0);
+            char p = in.charAt(2);
+
+            if ((o == 'A' && p == 'X') ||
+                    (o == 'B' && p == 'Y') ||
+                    (o == 'C' && p == 'Z')) {
+                return 3;
+            }
+
+            if ((o == 'A' && p == 'Y') ||
+                    (o == 'B' && p == 'Z') ||
+                    (o == 'C' && p == 'X')) {
+                return 6;
+            }
+
+            return 0;
+        };
+
+        var score = Arrays.stream(input.split("\n"))
+                .map( // map win/loss logic to existing player input
+                        l -> {
+                            switch (l.charAt(2)) {
+                                case 'X' -> { // lose
+                                    return switch (l.charAt(0)) {
+                                        case 'A' -> "A Z";
+                                        case 'B' -> "B X";
+                                        case 'C' -> "C Y";
+                                        default -> throw new IllegalStateException("Unexpected value: " + l.charAt(0));
+                                    };
+                                }
+                                case 'Y' -> { // Tie
+                                    return switch (l.charAt(0)) {
+                                        case 'A' -> "A X";
+                                        case 'B' -> "B Y";
+                                        case 'C' -> "C Z";
+                                        default -> throw new IllegalStateException("Unexpected value: " + l.charAt(0));
+                                    };
+                                }
+                                case 'Z' -> { // Win
+                                    return switch (l.charAt(0)) {
+                                        case 'A' -> "A Y";
+                                        case 'B' -> "B Z";
+                                        case 'C' -> "C X";
+                                        default -> throw new IllegalStateException("Unexpected value: " + l.charAt(0));
+                                    };
+                                }
+                                default -> throw new IllegalStateException("Unexpected value: " + l.charAt(2));
+                            }
+                        }
+                ).map(l -> {
+                            char player = l.charAt(2);
+                            int scoreFromPlay = switch (player) {
+                                case 'X' -> 1;
+                                case 'Y' -> 2;
+                                case 'Z' -> 3;
+                                default -> throw new IllegalStateException("Unexpected value: " + player);
+                            };
+                            System.out.println(l + " " + scoreFromPlay + " " + wonScore.apply(l));
+                            return scoreFromPlay + wonScore.apply(l);
+                        }
+                ).mapToInt(Integer::intValue).sum();
 
         System.out.println(score);
     }
