f02bb20c060f03d89d8d74ed29e428a6bf6a41ec
Tom Troyer <tom.troyer@gmail.com>
2022-12-05 23:20:21 -0500

    Adding Day3 solution part 1

--- src/main/java/Day3/MainTest.java
+++ src/main/java/Day3/MainTest.java
@@ -1,7 +1,35 @@
 package Day3;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class MainTest {
+    @Test
+    public void testSimpleInput() {
+        assertThat(new Main().run(
+                """
+                        vJrwpWtwJgWrhcsFMMfFFhFp
+                        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                        PmmdzqPrVvPwwTWBwg
+                        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                        ttgJtRGJQctTZtZT
+                        CrZsJsPPZsGzwwsLwLmpwMDw
+                        """
+        )).isEqualTo(157);
+    }
 
+    @Test
+    public void testGetValueOfChar() {
+        Main main = new Main();
+
+        assertThat(main.getValueOfChar('a'))
+                .isEqualTo(1);
+        assertThat(main.getValueOfChar('z'))
+                .isEqualTo(26);
+        assertThat(main.getValueOfChar('A'))
+                .isEqualTo(27);
+        assertThat(main.getValueOfChar('Z'))
+                .isEqualTo(52);
+    }
 }
